0c814931f521dabed1137716cc38bff5bf23fd10
jonathanpsantos <jonathan.santos194@etec.sp.gov.br>
2022-10-31 20:21:19 -0300

    aula 31-10

---
+++ Atividade_05.php
@@ -0,0 +1,37 @@
+<?php 
+	
+	$Idade = 14 ;
+	$sexo = 1 ; 
+	$altura = 1.25 ;
+	$cont ;
+	$maioridade = $Idade + 0;
+
+	// Echo "a maioridade é " . $maioridade;
+
+
+	for ($cont=0; $cont <=10 ; $cont++);
+
+	if ($sexo == 1 ) {
+			
+		$sexo++;
+
+		echo "Você e uma menina ". $cont;
+
+		}
+
+		elseif ($sexo == 2 ) {
+		
+		$sexo++;
+
+		echo "<br/> Matcho alfa ". $cont;
+		}
+		else{
+			echo "Você n tem sexo";
+
+		}
+
+		
+
+ ?>
+
+ 
